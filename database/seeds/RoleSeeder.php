3357a9a8bc99a7df8a4a30f9b3be285c778c83a5
florian <zobeleflorian@gmail.com>
2018-04-05 22:21:57 +0200

    role seeder

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('role')->insert([
+            'role_id' => 1,
+            'label' => 'ADMIN'
+        ]);
+
+        DB::table('role')->insert([
+            'role_id' => 2,
+            'label' => 'STUDENT'
+        ]);
+    }
+}
