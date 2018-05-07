8671764bab8b9c87fcc23d9110d02a6b3b6ebe8b
jeremyrts <jeremy.ratsimandresy@gmail.com>
2018-05-07 12:46:05 +0200

    add grade

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -7,8 +7,6 @@ use App\Models\Grade;
 
 class GradeController extends Controller
 {
-
-
    /**
 	 * Fetch all the Grade of the database
 	 * @return array<Grade> list of instance Grade
@@ -19,24 +17,10 @@ class GradeController extends Controller
 		return $grades;
 	}
 
-
-	/**
-	 * Fetch all the Grade for one student
-     * Fold by semester then UE and subject
-	 * @return array<Grade> list of instance grade
-	 */
-	public static function getStudentGradeFolded($student_id) {
-		$i=0;
-		$tab = array();
-		$pdo = MyPDO::getInstance()->prepare("SELECT G.grade, S.name FROM Grade as G INNER JOIN Subject as S ON G.subject_id = S.subject_id INNER JOIN UE as U ON S.UE_id = U.UE_id
-        WHERE G.student_id = :student_id ORDER BY U.semester, U.name");
-		$pdo->execute();
-		$pdo->setFetchMode(PDO::FETCH_CLASS,"Grade");
-		while(($ligne = $pdo->fetch()) != false){
-			$tab[$i]=$ligne;
-			$i++;
-		}
-		return $tab;
-	}
+	public static function addGrade(Request $request){
+        Subject::insert(
+            ['grade' => $request->input('grade'), 'coefficient' => $request->input('coefficient'),'subject_id' => $request->input('subject_id')]
+		);
+    }
 
 }
