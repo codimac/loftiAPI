1c9452fa6b2f572971998344a5b96d40819dfc3e
aubanemarquer <aubane.marquer@gmail.com>
2018-05-10 14:29:23 +0200

    getSubjectById terminé (1)

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -11,14 +11,28 @@ class SubjectController extends Controller {
 
 	public function getSubjectsByUE($ue_id){
 
- 
+
     	$subject = Subject::where('ue_id', '=', $ue_id)->get();
+    	return response()->json($subject);
+
+    	/*foreach($subjects as $subject){
+		  echo $subject->name; 
+		}
+
+    	$json_data = json_decode($subjects, true);
+		foreach($subject as $v){
+		   echo $v['name'].'<br>';
+		}*/
+    	
+
+    	/*$subjectArray = (array)$subject;
+		$subjectArray = array_filter($subjectArray);
  
-    	if(!$subject){
+    	if(empty($subjectArray)){
     		return response()->json(['error' => 'Cant find that UE.'], 400);
     	}else{
     		return response()->json($subject);
-    	}
+    	}*/
  
 	}
     
