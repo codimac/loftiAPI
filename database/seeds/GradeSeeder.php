e2c6de7176c2a86f3a073c20ac9d370533b510be
jeremyrts <jeremy.ratsimandresy@gmail.com>
2018-05-18 01:12:01 +0200

    fixed grade seeding by removing grade.coefficient

--- database/seeds/GradeSeeder.php
+++ database/seeds/GradeSeeder.php
@@ -18,7 +18,6 @@ class GradeSeeder extends Seeder
         for ($i=0; $i<500; $i++) {
             DB::table('grade')->insert([
                 'grade' => $faker->numberBetween($min = 0, $max = 20),
-                'coefficient' => $faker->numberBetween($min = 1, $max = 6),
                 'subject_id' => $faker->numberBetween($min = 1, $max = 69),
                 'student_id' => $faker->numberBetween($min = 1, $max = 150),
                 'assignment_id' => $faker->numberBetween($min = 1, $max = 4),
